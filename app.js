585b3ad0c067cd4264f7f0d89a56caaa97407a42
Victoria Frantsuh <frantsuhvika@gmail.com>
2020-06-23 12:10:57 +0300

    added userdb.js and studentdb

--- app.js
+++ app.js
@@ -1,30 +1,46 @@
-const express = require("express")
-const bodyParser=require("body-parser")
+const express = require("express");
+const bodyParser=require("body-parser");
 //for dir
-const path = require("path")
-const port= process.env.PORT || 5000
-const publicDir = path.join(__dirname, './public')
+const path = require("path");
+//db
+const db = require('./user_db');
+
+const port= process.env.PORT || 5000;
+const publicDir = path.join(__dirname, './public');
+
 //for dir
-const app = express()
+const app = express();
 
 
-app.use(express.static(publicDir))
+
+app.use(express.static(publicDir));
+
 //console.log(__dirname)
 //************ */
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({extended:true}));
 
 //connect .hbs file
-app.set('view engine', 'hbs')
+app.set('view engine', 'hbs');
+///
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.addUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
+
+
 
 //add pages and use pages
 app.get('/', (req,res) => {
-   res.render('index')
+  res.render('index')
 })
 app.get('/register', (req,res) => {
-    res.render('register')
+   res.render('register')
 }) 
 app.post('/register', (req,res) => {
-    console.log(req.body)
+
+   //console.log(req.body)
+   //console.log(res.body)
 }) 
 app.get('/login', (req,res) => {
     res.render('login')
@@ -33,5 +49,6 @@ app.post('/login', (req,res) => {
     console.log(req.body)
 })  
 app.listen(port, (req, res)=>{
-    console.log("Server start")
+ console.log("Server start")
 })
+//************************************* */
