5446d8a6a65632f619eaa15b536f036b0cedc251
Victoria Frantsuh <frantsuhvika@gmail.com>
2020-06-26 13:42:48 +0300

    added auth.js for register form

---
+++ controllers/auth.js
@@ -0,0 +1,43 @@
+
+const Pool = require('pg').Pool
+
+
+const pool = new Pool({
+  user: 'admin',
+  host: 'localhost',
+  database: 'school',
+  password: '',
+  port: 5432,
+})
+
+exports.register = (req,res) => {
+    console.log(req.body);
+    
+
+    const { fname, lname, email, password, cpassword} = req.body
+    //'SELECT * FROM users WHERE name = $1', 'eboko1@gmail.com')
+    pool.query('SELECT email FROM students WHERE email = $1', [email], (error, results) => {
+      if (error) {
+        throw error
+      }
+      
+      console.log('result:', results.rows);
+      console.log('results length ' , results.rows.length);
+
+      if (results.rows.length > 0){
+        
+        return res.render('register', {message: '*Цей email вже використовується'});
+
+      } 
+       if (password !== cpassword){
+          return res.render('register', {message: '*Паролі не збігаються'});
+      }
+      // add database insert into
+    })
+    
+    
+   // res.send ('Your registered');
+   res.render('login');
+
+   
+}
